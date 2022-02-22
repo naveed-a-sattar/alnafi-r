692ee3cb7c0d57c280007744f3386fca0ff43a1a
Naveed A. Sattar <naveed@techeguys.com>
2022-02-22 20:25:34 +0500

    more lines added

--- anotation.R
+++ anotation.R
@@ -27,4 +27,17 @@ x <-c(1+9i, 2+4i)
 class (x) ## complex
 
 x <- vector("numeric", length = 10)
-x
+x ## empty vector
+
+m <- matrix(nrow = 2, ncol =3)
+m ## matrix can be defined in R but in actual these are vectors and just presented like matrix.
+
+dim (m) ## gives dimensions of matrix 
+
+m<-1:10 ## create and vector of series from 1 to 10
+m 
+
+dim (m) <- c(2,5) ## converts m into a matrix of 2 rows and 5 columns
+m  ## its vice versa can also be created because total entries will remains 10. as dim(m) <- c(5,2)
+dim(m) <- c(5,2)
+m
