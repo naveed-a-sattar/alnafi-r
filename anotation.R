5c9f659fc437ee81b7ea54ef253eec64d82ff9ab
Naveed A. Sattar <naveed@techeguys.com>
2022-02-24 18:59:56 +0500

    new lines added

--- anotation.R
+++ anotation.R
@@ -41,3 +41,63 @@ dim (m) <- c(2,5) ## converts m into a matrix of 2 rows and 5 columns
 m  ## its vice versa can also be created because total entries will remains 10. as dim(m) <- c(5,2)
 dim(m) <- c(5,2)
 m
+
+x <- 1:3
+y <- 10:12
+
+cbind (x, y) ## to join two matrices of same size only, with column names
+
+rbind (x , y) ##with row name
+
+## list , its not vector
+# to create multi type values in a like vector format, but not a vector
+
+## if we give multi type values in a vector c() it will create vector but it will be converted to all values of same type / class. However in List each entry will maintain its own type.
+## not type casted
+
+x <- list (1, "a", TRUE , 1 +4i)
+x
+
+## Factors , is used when data is classified and divided.
+## for example values of vectors are labeled
+
+x <- factor(c("yes", "yes","no","yes","no"))
+x
+
+table(x) # it gives labels counts
+
+unclass(x)
+
+
+f <- factor(c("Punjab","KPK","Sindh", "Punjab"))
+f
+
+unclass(f) # definces levels it self.
+
+x  <- factor(c("yes", "yes","no","yes","no"), levels= c("yes","no"))
+x
+## assignment
+x <- factor(c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+x
+unclass(x)
+
+x <- factor(c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"), levels= c("Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"))
+x
+unclass(x)
+
+## Missing values
+x<-c(1,2, NA, 10,3)
+is.na(x)
+
+is.nan(x) ## NaN and NA are different, NA considera NaN as NA but NAN cannot consider NA as NAN
+
+x <- c(1,2,NaN, NA, 4)
+x
+
+is.na(x) #[1] FALSE FALSE  TRUE  TRUE FALSE
+is.nan(x) #[1] FALSE FALSE  TRUE FALSE FALSE
+
+## Data Frames, it is used to store lists in table, in form of columns with column headings
+## column lengths should be same
+
+## to create data Frames, read.table() and read.csv(), can be converted into matrix.
